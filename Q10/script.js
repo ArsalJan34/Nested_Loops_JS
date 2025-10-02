f15f1a029a1ffbdaee34554b0f6838d6f4e13926
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-02 18:13:49 +0500

    Changes made

--- Q10/script.js
+++ Q10/script.js
@@ -1,6 +1,6 @@
-for (let j = 0; j < arr[0].length; j++) {
-  let col = "";
-  for (let i = 0; i < arr.length; i++) {
+for (var j = 0; j < arr[0].length; j++) {
+  var col = "";
+  for (var i = 0; i < arr.length; i++) {
     col += arr[i][j] + " ";
   }
   console.log(col);
