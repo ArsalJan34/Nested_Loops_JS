fe66761873b4c0b27c010159b922dd07c1c24bff
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-02 17:11:40 +0500

    Changes made

---
+++ Q3/script.js
@@ -0,0 +1,9 @@
+
+    // i have to print an pyrmid of 4 rows with * 1 2 3 4 in each row 
+for (var i = 0; i < 4; i++) {
+    var row = "";
+    for(var j = 1; j <=4; j++){
+        row += "* " + j;
+    }
+    console.log(row);
+}
