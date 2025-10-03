511d1bb56e3f663a293262268fefc9ec47dce6c8
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-03 17:15:47 +0500

    Changes made

---
+++ Q19/script.js
@@ -0,0 +1,18 @@
+var arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [10,11,12],
+  [13,14,15],
+  [16,17,18],
+  [19,20,21]
+];
+var count= 0;
+for(var i = 0; i < arr.length; i++){
+    for(var j =0; j <arr[i].length; j++){
+        if(arr[i][j] % 5 === 0){
+            count++;
+        }
+    }
+}
+console.log(count)
