8eba295215d7ef4771c97422368cee9a005a2d12
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-03 17:09:51 +0500

    Changes made

--- Q17/script.js
+++ Q17/script.js
@@ -2,7 +2,8 @@ var arr = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
-  [10,11,12]
+  [10,11,12],
+  [13,14,15]
 ];
 var count= 0;
 for(var i = 0; i < arr.length; i++){
@@ -11,6 +12,6 @@ for(var i = 0; i < arr.length; i++){
             count++;
         }
     }
-    
+
 }
 console.log(count)
