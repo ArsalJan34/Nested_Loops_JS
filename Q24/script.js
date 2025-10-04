9da44cfed53eacf6b154e9c2a1a5f241ba89f222
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-04 10:32:21 +0500

    Changes made

---
+++ Q24/script.js
@@ -0,0 +1,24 @@
+var arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [10,11,12],
+  [13,14,15],
+  [16,17,18],
+  [19,20,21]
+];
+
+var minSum = 0;
+var minRowIndex = 0;
+
+for (var i = 0; i < arr.length; i++) {
+  var rowSum = 0;
+  for (var j = 0; j < arr[i].length; j++) {
+    rowSum += arr[i][j];
+  }
+  if (rowSum < minSum) {
+    minSum = rowSum;
+    minRowIndex = i;
+  }
+}
+console.log( minRowIndex + 1, " largest sum =", minSum);
